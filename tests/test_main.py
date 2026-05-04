35d0dc0e4a1167c6580960314e4fa70638069e62
jmcampos-sec <jmcamposenrique@gmail.com>
2026-05-05 00:31:59 +0200

    fix: eliminar import pytest no utilizado en tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ requiring a running server.
 Author: Juan Manuel Campos Enrique / Mónica Penacho
 """
 
-import pytest
+# import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
