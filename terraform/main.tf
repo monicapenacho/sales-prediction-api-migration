a40c5b67b9a5f22b897558ab552e7f5aece14f7a
monicapenacho <monicapenachocollado@gmail.com>
2026-05-05 22:07:58 +0200

    fix(terraform): desplegar imagen propia desde ACR + ignorar tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_container_group" "api" {
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   ip_address_type     = "Public"
-  dns_name_label      = "abc-analytics-api"
+  dns_name_label      = "abc-analytics-api-juanmonica"
 
   restart_policy = "Always"
 
